49c589fa8bbf3c80bf7c55d5ffb1bcddd0ba25e6
Kyle Hodgson <kyle.hodgson@gmail.com>
2013-01-07 03:25:02 +0000

    adding fake repository

--- ServiceStackJQueryMobileTutorial/ServiceStackJQueryMobileTutorial/Model/Event.cs
+++ ServiceStackJQueryMobileTutorial/ServiceStackJQueryMobileTutorial/Model/Event.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,13 @@ namespace ServiceStackJQueryMobileTutorial.Model
     public class Event
     {
         public int? Page { get; set; }
+
         public string SearchTerm { get; set; }
+
+        public DateTime EventStart { get; set; }
+
+        public string Speaker { get; set; }
+
+        public string Subject { get; set; }
     }
 }
---
+++ ServiceStackJQueryMobileTutorial/ServiceStackJQueryMobileTutorial/Repository/EventRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStackJQueryMobileTutorial.Model;
+
+namespace ServiceStackJQueryMobileTutorial.Repository
+{
+    public enum EventRepositoryResponse
+    {
+        Success,
+        DateAlreadyPresent
+    }
+
+    public class EventRepository
+    {
+        private List<Event> _repo;
+        public EventRepository()
+        {
+            _repo = new List<Event>();
+            InitializeTestData();
+        }
+
+        public EventRepository(object repo)
+        {
+            _repo = repo as List<Event>;
+        }
+
+        internal List<Event> GetEvents()
+        {
+            return _repo;
+        }
+
+        internal Event GetEvent(string subject)
+        {
+            return (_repo.Where(e => e.Subject.ToLower() == subject.ToLower())).FirstOrDefault();
+        }
+
+        internal EventRepositoryResponse SaveEvent(Event eve)
+        {
+            var count = (from e in _repo where e.EventStart == eve.EventStart select e).Count();
+            if (count > 0) return EventRepositoryResponse.DateAlreadyPresent;
+            _repo.Add(eve);
+            return EventRepositoryResponse.Success;
+        }
+
+        internal int GetCountOfEvents()
+        {
+            return GetCountOfEvents(null);
+        }
+
+        internal int GetCountOfEvents(string searchTerm)
+        {
+            if (searchTerm == null) return _repo.Count;
+            return _repo.Count(e => (e.Subject.Contains(searchTerm)));
+        }
+
+        internal void InitializeTestData()
+        {
+            int recordsToMake = 20;
+            for (int i = 1; i < recordsToMake; i++)
+            {
+                _repo.Add(
+                    new Event
+                    {
+                        EventStart = new DateTime(2012, 4, i),
+                        Speaker = "Atley Hunter",
+                        Subject = "WP7 Programming"
+                    });
+            }
+        }
+    }
+}
